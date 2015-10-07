9868dd97e2976188dfed0a769f11582f414075b0
1yinzhang <yiyin.zhang.15@ucl.ac.uk>
2015-10-07 22:04:24 +0100

    Create helloworldC.java

---
+++ helloworldC.java
@@ -0,0 +1,10 @@
+/**
+ * Created by zooey on 15/10/7.
+ */
+public class HelloWorldC {
+    public static void main (String args[]) {
+        char capitalC='c';
+        System.out.print("Hello world!");
+        System.out.print(capitalC);
+    };
+}
